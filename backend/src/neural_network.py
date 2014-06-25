aa36540bcbd1c778bdfbe638918a49bef11e3a63
Petr Bartunek <petr.bartunek@firma.seznam.cz>
2014-06-25 11:51:59 +0200

    Pridani testovacich metod get/add

--- backend/src/neural_network.py
+++ backend/src/neural_network.py
@@ -38,5 +38,72 @@ class NeuralNetworkBackend(Backend):
 
         return param
     #enddef
+
+    @rpcStatusDecorator('neural_network.get', 'S:i')
+    @MySQL_slave
+    def get(self, id):
+        """
+        Testovaci funkce
+
+        Signature:
+            neural_network.get(integer id)
+
+        @id                 neural_network_id
+
+        Returns:
+            struct {
+                int status              200 = OK
+                string statusMessage    Textovy popis stavu
+                struct data {
+                    integer id              neural_network_id
+                    string description      description
+                    string configuration    konfigurace
+                }
+            }
+        """
+
+        query = """
+            SELECT id, description, configuration
+            FROM neural_network
+            WHERE id = %s
+        """
+        self.cursor.execute(query, id)
+        neural_network = self.cursor.fetchone()
+        if not neural_network:
+            raise Exception(404, "Neural network not found")
+        #endif
+        return neural_network
+    #enddef
+
+    @rpcStatusDecorator('neural_network.add', 'S:S')
+    @MySQL_master
+    def add(self, param):
+        """
+        Testovaci funkce
+
+        Signature:
+            neural_network.add(struct param)
+
+        @param {
+            description     Popisek
+            configuration   Konfigurace neuronove site
+        }
+
+        Returns:
+            struct {
+                int status              200 = OK
+                string statusMessage    Textovy popis stavu
+                int data                Vraci id nove neuronove site
+            }
+        """
+
+        query = """
+            INSERT INTO neural_network (`description`, `configuration`)
+            VALUE (%(description)s, %(configuration)s)
+        """
+        self.cursor.execute(query, param)
+        neural_network_id = self.cursor.lastrowid
+        return neural_network_id
+    #enddef
 #endclass
 
