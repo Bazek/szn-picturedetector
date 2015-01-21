f801093da8ef148583c37cc725530133cae119b9
Ales Zurek <ales.zurek@gmail.com>
2015-01-21 17:27:13 +0100

    Procisteno util.py

--- common/src/picturedetector/util.py
+++ common/src/picturedetector/util.py
@@ -9,12 +9,8 @@
 # Copyright (C) Seznam.cz a.s. 2014
 # All Rights Reserved
 
-import sys
-sys.path.insert(0, '/usr/python')
 from caffe.proto import caffe_pb2
 from google.protobuf import text_format
-import os.path
-from dbglog import dbg
 
 # Konstanty pro rozliseni souboru pro uceni a validaci
 TRAIN = 'train'
@@ -23,10 +19,6 @@ VALIDATE = 'validate'
 # Konstanty, ktere urcuji nactene cesty ze souboru imagenet_train_val.prototxt
 SOURCE = 'source'
 MEAN_FILE = 'mean_file'    
-
-MEAN_FILE_PATH = '/www/picturedetector/caffe/neural_networks/meanfiles'
-MEAN_FILE_PREFIX = 'mean_file_'
-MEAN_FILE_EXTENSION = '.npy'
 
 def readProtoSolverFile(filepath):
     solver_config = caffe_pb2.SolverParameter()
@@ -95,12 +87,4 @@ def parseLayerPaths(proto):
     #endfor
 
     return results
-#enddef
-
-def getMeanFilePath(neural_network_id):
-    base = MEAN_FILE_PATH
-    prefix = MEAN_FILE_PREFIX
-    extension = MEAN_FILE_EXTENSION
-    
-    return os.path.join(base, prefix + str(neural_network_id) + extension)
 #enddef
